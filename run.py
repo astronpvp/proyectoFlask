ad7b0a34fef6946c64f7b331b9bffc119f7e4bd9
astronpvp <98805055+astronpvp@users.noreply.github.com>
2025-06-12 21:09:45 +0200

    cambio de puerto para deploy

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app import create_app
+import os
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # 👈 usa el puerto que Render proporciona
+    app.run(host="0.0.0.0", port=port)        # 👈 escucha en todas las interfaces
